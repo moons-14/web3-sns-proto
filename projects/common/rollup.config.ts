ad802cf34b492940df700b18ba7eb288fb58e5cd
Inaridiy <inainainariinainari@gmail.com>
2022-08-20 16:36:10 +0000

    @crypteen/commonで型も生成できるように

--- projects/common/rollup.config.ts
+++ projects/common/rollup.config.ts
@@ -1,10 +1,18 @@
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    dir: "dist",
-    format: "cjs",
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist",
+      format: "cjs",
+    },
+    plugins: [typescript()],
   },
-  plugins: [typescript()],
-};
+  {
+    input: "./dist/src/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
