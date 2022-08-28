48c3a42369f3eee17fdb1d03ec744680869decc1
inaridiy <inainainariinainari@gmail.com>
2022-08-29 00:55:09 +0900

    CreateModalを実装

--- projects/interface/src/components/Modal/CreateChannelModal.tsx
+++ projects/interface/src/components/Modal/CreateChannelModal.tsx
@@ -1,1 +1,14 @@
-export const CreateChannelModal = () => {};
+import { Modal, ModalProps, ModalTitle } from "../Elements";
+
+const CreateChannelForm = () => {
+  return <form></form>;
+};
+
+export const CreateChannelModal: React.FC<ModalProps> = (props) => {
+  return (
+    <Modal className="flex flex-col gap-2 p-4 sm:p-6" {...props}>
+      <ModalTitle className="text-lg font-bold">Create New Chat</ModalTitle>
+      <CreateChannelForm />
+    </Modal>
+  );
+};
--- projects/interface/src/features/chat/routes/Channels.tsx
+++ projects/interface/src/features/chat/routes/Channels.tsx
@@ -3,27 +3,39 @@ import {
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
 
+import { CreateChannelModal } from "@/components/Modal/CreateChannelModal";
+import { useModal } from "@/states/modal";
+
 export const Channels = () => {
+  const { close, isOpen, open } = useModal("create-channel");
   return (
-    <div className="flex w-full flex-col gap-2 p-4">
-      <div className="bg-base-100 card flex-row gap-2 p-4">
-        <MagnifyingGlassIcon className="h-6 w-6" />
-        <input
-          type="text"
-          placeholder="Search in message"
-          className="w-full outline-none"
-        />
+    <>
+      <CreateChannelModal open={isOpen} onClose={close} />
+      <div className="flex w-full flex-col gap-2 p-4">
+        <div className="bg-base-100 card flex-row gap-2 p-4">
+          <MagnifyingGlassIcon className="h-6 w-6" />
+          <input
+            type="text"
+            placeholder="Search in message"
+            className="w-full outline-none"
+          />
+        </div>
+        <div className="flex gap-2 overflow-x-auto whitespace-nowrap px-4 pb-2">
+          <div className="btn btn-sm btn-outline">All</div>
+          <div className="btn btn-sm btn-outline btn-primary">
+            Direct Message
+          </div>
+          <div className="btn btn-sm btn-outline btn-secondary">Important</div>
+          <div className="btn btn-sm btn-outline btn-info">Unknown</div>
+        </div>
+        <button
+          className="btn btn-info fixed bottom-24 right-6 gap-2 rounded-full shadow-lg"
+          onClick={open}
+        >
+          <ChatBubbleLeftRightIcon className="h-6 w-6" />
+          Start Chat
+        </button>
       </div>
-      <div className="flex gap-2 overflow-x-auto whitespace-nowrap px-4 pb-2">
-        <div className="btn btn-sm btn-outline">All</div>
-        <div className="btn btn-sm btn-outline btn-primary">Direct Message</div>
-        <div className="btn btn-sm btn-outline btn-secondary">Important</div>
-        <div className="btn btn-sm btn-outline btn-info">Unknown</div>
-      </div>
-      <div className="btn btn-info fixed bottom-24 right-6 gap-2 rounded-full shadow-lg">
-        <ChatBubbleLeftRightIcon className="h-6 w-6" />
-        Start Chat
-      </div>
-    </div>
+    </>
   );
 };
