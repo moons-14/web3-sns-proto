c14a75aea283788b0ebd57ce3d5ba61d7aa8f963
inaridiy <inainainariinainari@gmail.com>
2022-08-29 02:52:22 +0900

    テーマをフォールバックにも実装

--- projects/interface/src/providers/fallback.tsx
+++ projects/interface/src/providers/fallback.tsx
@@ -1,20 +1,20 @@
-import { Spinner } from "@/components/Elements";
+import { Spinner, ThemeBox } from "@/components/Elements";
 
 export const LoadingFallback = () => {
   return (
-    <div className="bg-base-200 flex h-screen w-screen flex-col items-center justify-center">
+    <ThemeBox className="bg-base-200 flex h-screen w-screen flex-col items-center justify-center">
       <Spinner size="lg" />
-    </div>
+    </ThemeBox>
   );
 };
 
 export const ErrorFallback = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-6">
+    <ThemeBox className="flex h-screen w-screen flex-col items-center justify-center gap-6">
       <h1 className="text-error-content bg-base-200 text-center text-2xl font-bold sm:text-4xl">
         Something went wrong
       </h1>
       <button className="btn btn-error">Back To Home</button>
-    </div>
+    </ThemeBox>
   );
 };
