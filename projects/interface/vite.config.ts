61e46362515e4a5f8e39ed5985ded4fea0a0167f
inaridiy <inainainariinainari@gmail.com>
2022-08-29 04:45:24 +0900

    ゼロに戻す

--- projects/interface/vite.config.ts
+++ projects/interface/vite.config.ts
@@ -1,40 +1,18 @@
 import react from "@vitejs/plugin-react";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 import { defineConfig } from "vite";
-
-const production = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    !production &&
-      (nodePolyfills({
-        include: [
-          "node_modules/**/*.js",
-          new RegExp("node_modules/.vite/.*js"),
-        ],
-      }) as any),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@/": `${__dirname}/src/`,
-      http: "http-browserify",
-      https: "https-browserify",
     },
   },
 
   server: {
     watch: {
       usePolling: true, // for WSL
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [nodePolyfills() as any],
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
     },
   },
 });
